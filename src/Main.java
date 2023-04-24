4b124093c6f3df2a09c62f6f4fe1d08ca5ea07dc
Caio <caiobulha@gmail.com>
2023-04-24 14:42:41 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        for (String car: cars) {
+            System.out.println(car);
+        }
     }
 }
